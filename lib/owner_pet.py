60d6bb5d7053d2f84e0ab7169728bac0dffe8175
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-02-23 15:51:45 -0500

    Took some time, but I got it!

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner = None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("You have to pick the correct PET_TYPE")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.owner_pets = []
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("The argument must be of type Pet")
+        pet.owner = self
+        self.owner_pets.append(pet)
+
+    def get_sorted_pets(self):
+        sorted_pets = sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted_pets
+
